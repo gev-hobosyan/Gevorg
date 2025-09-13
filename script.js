2d48001ed861ae7da7a64a3863539d3ff83343f0
Argo <hobosyangevorg2008@gmail.com>
2025-09-13 20:30:42 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let currentUserName;
+
+let name = "Ilya"
+
+console.log( `hello ${1}` ); // hello 1
+
+console.log( `hello ${"name"}` ); // hello name
+
+console.log( `hello ${name}`); // hello Ilya
+
+let userName = 12345; //պետք է լինի անուն, string փոփոխական օր` "John"
+let userAge = "шестнадцать"; // պետք է լինի թվի տեսքով` 16
+let isOnline = "yes"; // պետք է լինի bool տիպի փոփոխական` true
+let score = true; // պետք է թիվ լինի օր.`12
